e900ef714492bd836d156231fa3afaad9b280a44
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2024-12-03 16:02:02 +0200

    Dependencies added

---
+++ ConnectHub/src/main/java/com/mycompany/connecthub/ConnectHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.connecthub;
+
+/**
+ *
+ * @author habib
+ */
+public class ConnectHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! IM Habiba");
+    }
+}
