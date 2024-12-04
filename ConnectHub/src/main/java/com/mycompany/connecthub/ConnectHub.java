2f5360a6c30e709252b84455a3544ae3172721db
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2024-12-04 05:34:30 +0200

    Added Some Classes

--- ConnectHub/src/main/java/com/mycompany/connecthub/ConnectHub.java
+++ ConnectHub/src/main/java/com/mycompany/connecthub/ConnectHub.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.connecthub;
 
-/**
- *
- * @author habib
- */
 public class ConnectHub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! IM Habiba");
+        //System.out.println("Hello World! IM Habiba");
     }
 }
