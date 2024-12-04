52b5107a6fab9a0bb746488583b83d6bd0e06f4d
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2024-12-04 08:52:51 +0200

    Nth Major edited

--- ConnectHub/src/main/java/Backend/UserManager.java
+++ ConnectHub/src/main/java/Backend/UserManager.java
@@ -1,35 +1,38 @@
 package Backend;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
+
 
 public class UserManager implements UserManagerInterface {
 
     private ArrayList<User> users;
-    
+
     public UserManager() {
         this.users = new ArrayList<>();
     }
 
-    public  ArrayList<User> getUsers() {
+    public ArrayList<User> getUsers() {
         return users;
     }
-    
+
     @Override
     public void signup(String userID, String email, String username, String password, LocalDate dateOfBirth) {
         if (!findUserByID(userID)) {
-            User u = new User(userID, email, username, password, dateOfBirth);
+            User u = new User(userID, email, username, dateOfBirth, password);
             users.add(u);
+            u.setStatus("Online");
         }
     }
-
-    
 
     @Override
     public void login(String email, String password) {
         for (User u : users) {
             if (email.equals(u.getEmail()) && password.equals(u.getPassword())) {
-                u.setStatus("online");
+                u.setStatus("Online");
             }
         }
     }
