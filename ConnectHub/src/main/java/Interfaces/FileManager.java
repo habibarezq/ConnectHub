cdfe6c46ffd8bc627b1c77bc541a14b364c674d1
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2024-12-05 12:29:47 +0200

    return type is changed to void

--- ConnectHub/src/main/java/Interfaces/FileManager.java
+++ ConnectHub/src/main/java/Interfaces/FileManager.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 public interface FileManager<T> {
 
-    // methods to be implemented to save and reaf from JSON Files
-    public ArrayList<T> readFromFile(String str);
+    // Methods to be implemented to save and read from JSON Files
+    public void readFromFile(String FILE_PATH);
     public void saveTOFile(ArrayList<T> data , String FILE_PATH);
 
 }
