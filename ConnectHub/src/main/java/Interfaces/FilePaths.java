3719cefa0fb55108829c97d50c1edcbba1a27f35
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2024-12-05 12:30:04 +0200

    changed variable name

--- ConnectHub/src/main/java/Interfaces/FilePaths.java
+++ ConnectHub/src/main/java/Interfaces/FilePaths.java
@@ -1,13 +1,10 @@
-
 package Interfaces;
 
-
 public interface FilePaths {
-    
-    static final String USERS_FILE_PATH = "users.txt";
-    
-    static final String PROFILES_FILE_PATH = "profiles.txt";
-    
+
+    public String USERS_FILE_PATH = "users.txt";
+
+    public String PROFILES_FILE_PATH = "profiles.txt";
+
     //static final String POSTS_FILE_PATH = "posts.txt";
-    
 }
