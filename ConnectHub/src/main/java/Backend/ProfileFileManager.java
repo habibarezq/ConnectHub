2c94079020e76768e95c98fd43bc909db2667017
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-06 05:09:14 +0200

    modified ProfileFileManager

--- ConnectHub/src/main/java/Backend/ProfileFileManager.java
+++ ConnectHub/src/main/java/Backend/ProfileFileManager.java
@@ -1,4 +1,3 @@
-
 package Backend;
 
 import java.io.*;
@@ -10,18 +9,97 @@ import org.json.*;
 
 //importing the FilePaths and FileManager interfaces
 import Interfaces.*;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import javax.imageio.ImageIO;
 
-public class ProfileFileManager implements FileManager<UserProfile>{
+public class ProfileFileManager implements FileManager<UserProfile> {
 
+    private static ProfileFileManager instance;
+    private ArrayList<UserProfile> profiles;
+    private String FILE_PATH = FilePaths.PROFILES_FILE_PATH;
 
-    @Override
-    public void saveToFile(ArrayList<UserProfile> data) {
-        
+    // private constructor to avoid instantiation
+    private ProfileFileManager() {
+        this.profiles = new ArrayList<>();
+        readFromFile();
     }
+
+    public static ProfileFileManager getInstance() {
+        if (instance == null) {
+            instance = new ProfileFileManager();
+        }
+        return instance;
+    }
+
+    public ArrayList<UserProfile> getProfiles() {
+        if (profiles.isEmpty()) {
+            readFromFile(); //EH EL LOGIC HENAA
+        }
+        return profiles;
+    }
+
 
     @Override
     public void readFromFile() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (!profiles.isEmpty()) {
+            return; // To avoid reloading
+        }
+        try {
+            String json = new String(Files.readAllBytes(Paths.get(FILE_PATH)));
+            JSONArray postsArray = new JSONArray(json); // Parse the JSON array
+
+            this.profiles.clear(); // Clear the existing list before loading new data
+            for (int i = 0; i < postsArray.length(); i++) {
+                JSONObject postJSON = postsArray.getJSONObject(i);
+                String userId = postJSON.getString("userId");
+                String bio = postJSON.getString("Bio");
+                String profilePath = postJSON.getString("Profile photo Path");
+                String coverPath = postJSON.getString("Cover photo Path");
+
+                profiles.add(new UserProfile(userId, profilePath, coverPath , bio));
+            }
+        } catch (IOException ex) {
+            System.out.println("Error reading file: " + ex.getMessage());
+        } catch (JSONException ex) {
+            System.out.println("Error parsing JSON: " + ex.getMessage());
+        }
     }
-    
+
+     @Override
+    public void saveToFile(ArrayList<UserProfile> data) {
+        File f = new File(FILE_PATH);
+        try {
+            f.createNewFile();
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+        JSONArray postsArray = new JSONArray();
+        for (UserProfile profile : data) {
+            JSONObject postJSON = new JSONObject();
+            postJSON.put("userId", profile.getUserId());
+            postJSON.put("Bio", profile.getBio());
+            postJSON.put("Profile photo Path", profile.getProfilePic());
+            postJSON.put("Cover photo Path", profile.getCoverPic());
+          
+            postsArray.put(postJSON);
+        }
+
+        try {
+            FileWriter file = new FileWriter(FILE_PATH);
+            file.write(postsArray.toString(4));
+            file.flush();
+            file.close();
+            System.out.println("Data Saved Successfully.");
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error saving posts: " + e.getMessage());
+        }
+    }
 }
+
