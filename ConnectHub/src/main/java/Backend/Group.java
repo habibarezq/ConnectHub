823e5890a5c0214f0e907f4b178b435f8c8c252a
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-13 00:05:50 +0200

    edited constructor

---
+++ ConnectHub/src/main/java/Backend/Group.java
@@ -0,0 +1,54 @@
+package Backend;
+
+import java.util.*;
+
+public class Group {
+    
+    private String name;
+    private String description;
+    private String photoPath;
+    private String groupId;
+    
+    private String creatorId;       // id of the creator "Primary admin" of the group
+    private ArrayList<User> admins; // List of normal admins
+    private ArrayList<User> users ; // List of normal users
+
+    public Group(String name, String description, String photoPath, String groupId, String creatorId) {
+        this.name = name;
+        this.description = description;
+        this.photoPath = photoPath;
+        this.groupId = groupId;
+        this.creatorId = creatorId;
+        this.admins = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public ArrayList<User> getAdmins() {
+        return admins;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+    
+}
