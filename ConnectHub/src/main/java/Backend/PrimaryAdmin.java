05b1e90220dc879101c453f89d8d3c2e3ca36e64
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-12 23:02:26 +0200

    primary admins subclass

---
+++ ConnectHub/src/main/java/Backend/PrimaryAdmin.java
@@ -0,0 +1,11 @@
+
+package Backend;
+
+public class PrimaryAdmin extends Admins{
+    
+    
+    //promote/demote admins
+    //remove members  OVERRIDEN
+    //manage posts    OVERRIDEN
+    //delete group
+}
