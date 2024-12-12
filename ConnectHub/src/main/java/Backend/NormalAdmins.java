af5f8dccdb4fc784e7df87689fa49a0b27c541dd
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-12 23:23:43 +0200

    .

--- ConnectHub/src/main/java/Backend/NormalAdmins.java
+++
@@ -1,9 +0,0 @@
-
-package Backend;
-
-public class NormalAdmins extends Admins {
-    
-    //Approve/decline membership requests
-    //remove members  OVERRIDEN 
-    //manage posts    OVERRIDEN
-}
