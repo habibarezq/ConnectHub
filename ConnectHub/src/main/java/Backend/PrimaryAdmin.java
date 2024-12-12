7ca6e36d07eb0da2d9f95448f5af9d2fa051c971
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-12 23:27:20 +0200

    subclass for the primary admin "creator"

---
+++ ConnectHub/src/main/java/Backend/PrimaryAdmin.java
@@ -0,0 +1,10 @@
+package Backend;
+
+public class PrimaryAdmin extends Admins{
+    
+    
+    //promote/demote admins
+    //remove members  OVERRIDEN
+    //manage posts    OVERRIDEN
+    //delete group
+}
