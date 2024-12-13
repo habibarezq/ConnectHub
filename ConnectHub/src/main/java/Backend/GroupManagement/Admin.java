4963eb1f95f334115814f449d543491c52fd7452
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-13 03:19:19 +0200

    added a SuperClass admin

---
+++ ConnectHub/src/main/java/Backend/GroupManagement/Admin.java
@@ -0,0 +1,16 @@
+
+package Backend.GroupManagement;
+
+public abstract class Admin extends GroupUser {
+
+    public Admin(String groupUserId) {
+        super(groupUserId);
+        
+    }
+    
+    
+    //remove members 
+    //manage posts
+    
+}
+
