7fd9870249e9f02fa66cbeb8521035e46eddb7ef
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-13 03:20:37 +0200

    added a manager for the membership

---
+++ ConnectHub/src/main/java/Backend/GroupManagement/MembershipManager.java
@@ -0,0 +1,94 @@
+
+package Backend.GroupManagement;
+
+import Backend.FileManagers.FriendsFileManager;
+import Backend.FileManagers.GroupMembershipFileManager;
+import Backend.FileManagers.GroupsFileManager;
+import Backend.FileManagers.UserFileManager;
+import Backend.UserManagement.User;
+import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class MembershipManager {
+    
+    private final String groupId;
+
+    private ArrayList<NormalAdmin> admins ;
+    private ArrayList<GroupUser> groupUsers;
+
+    MembershipManager(String groupId) {
+        this.groupId = groupId;
+        this.admins = new ArrayList<>();
+        this.groupUsers = new ArrayList<>();
+        loadMembershipData();
+    }
+
+
+    private void loadMembershipData() {
+        JSONObject groupData = GroupMembershipFileManager.getInstance().getUserData(groupId);
+        if (groupData == null) {
+            return; // No data found for the user
+        }
+
+        JSONArray AdminsArray = groupData.optJSONArray("admins");
+        if (AdminsArray != null) {
+            for (int i = 0; i < AdminsArray.length(); i++) {
+                String adminId = AdminsArray.getString(i);
+               // User friend = UserFileManager.getInstance().findUserByID(adminId);
+               NormalAdmin admin = new NormalAdmin(adminId); 
+               if (admin != null) {
+                    admins.add(admin);
+                }
+            }
+        }
+
+        JSONArray usersArray = groupData.optJSONArray("users");
+        if (usersArray != null) {
+            for (int i = 0; i < usersArray.length(); i++) {
+                String userId = usersArray.getString(i);
+               // User blockedUser = UserFileManager.getInstance().findUserByID(blockedId);
+                GroupUser user = new GroupUser(userId);
+                if (user != null) {
+                    groupUsers.add(user);
+                }
+            }
+        }
+    }
+
+    public ArrayList<NormalAdmin> getAdmins() {
+        return admins;
+    }
+
+    public ArrayList<GroupUser> getGroupUsers() {
+        return groupUsers;
+    }
+
+   
+    
+    public void saveUserData() {
+        JSONObject userData = new JSONObject();
+        userData.put("groupId", groupId);
+
+        JSONArray adminsArray = new JSONArray();
+        for (NormalAdmin admin : admins) {
+            adminsArray.put(admin.getGroupUserId());
+        }
+        userData.put("admins", adminsArray);
+
+        JSONArray usersArray = new JSONArray();
+        for (GroupUser user : groupUsers) {
+            usersArray.put(user.getGroupUserId());
+        }
+        userData.put("users", usersArray);
+
+        GroupMembershipFileManager.getInstance().updateUserData(groupId, userData);
+         GroupMembershipFileManager.getInstance().saveToFile();
+    } 
+    //NOTE THAT The saveUserData() saves the user's friends and blocked lists back to the friends file. 
+    //It updates the corresponding JSON object for the user and saves it using FriendsFileManager.
+}
+
+    
+  
