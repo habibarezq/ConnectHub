253371ae2e366e62f72fe0c4b657556a09d0b78f
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2024-12-13 11:04:22 +0200

    Manages group creation

---
+++ ConnectHub/src/main/java/Backend/GroupManagement/GroupServiceManager.java
@@ -0,0 +1,22 @@
+package Backend.GroupManagement;
+
+import Backend.FileManagers.GroupsFileManager;
+import java.util.ArrayList;
+
+public class GroupServiceManager { //Greates the group only 
+
+    private static ArrayList<Group> allGroups;
+
+    public static Group createGroup(String name, String description, String photoPath, String creatorId) {
+        allGroups=GroupsFileManager.getInstance().getGroups();
+        Group group = new Group(name,description,photoPath,creatorId);
+        group.setGroupId(java.util.UUID.randomUUID().toString());
+        allGroups.add(group);
+        return group;
+    }
+
+    public static ArrayList<Group> getAllGroups() {
+        return allGroups;
+    }
+}
+
