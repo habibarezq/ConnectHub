46afc4db2ae3b6492eba4ebb28e6c978e5d93c02
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-13 17:21:03 +0200

    test code

--- ConnectHub/src/main/java/com/mycompany/connecthub/ConnectHub.java
+++ ConnectHub/src/main/java/com/mycompany/connecthub/ConnectHub.java
@@ -1,9 +1,10 @@
-
 package com.mycompany.connecthub;
 
 import Backend.*;
 import Backend.FileManagers.*;
 import Backend.GroupManagement.*;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -11,65 +12,72 @@ import org.json.JSONObject;
 public class ConnectHub {
 
     public static void main(String[] args) {
-        // Initialize the GroupsFileManager
-        GroupsFileManager groupsFileManager = GroupsFileManager.getInstance();
+         // Clear the file for a fresh start
+        clearFile();
 
-        // Read groups from the file
-        groupsFileManager.readFromFile();
+        // Initialize GroupContentManager for group1
+        String groupId1 = "group1";
+        GroupContentManager groupContentManager1 = GroupContentManager.getInstance(groupId1);
 
-        // Retrieve the list of groups
-        ArrayList<Group> groups = groupsFileManager.getGroups();
+        // Add new posts to group1
+        GroupPost post1 = new GroupPost(groupId1, "user1", "This is a test post 1 for group1", "path/to/image1.jpg", LocalDateTime.now());
+        GroupPost post2 = new GroupPost(groupId1, "user2", "This is a test post 2 for group1", "path/to/image2.jpg", LocalDateTime.now());
 
-        // Print the groups
-        if (groups.isEmpty()) {
-            System.out.println("No groups found.");
-        } else {
-            System.out.println("Groups loaded from file:");
-            for (Group group : groups) {
-                System.out.println("---------------------------------");
-                System.out.println("Group ID: " + group.getGroupId());
-                System.out.println("Creator ID: " + group.getCreatorId());
-                System.out.println("Name: " + group.getName());
-                System.out.println("Description: " + group.getDescription());
-                System.out.println("Photo Path: " + group.getPhotoPath());
-            }
-        }
-        //GroupServiceManager.createGroup("My Family Group", "Here we are a family", "C:\\Users\\habib\\Documents\\NetBeansProjects\\ConnectHub\\ConnectHub\\pics\\download (2).jpeg", "69890c7f-4ed0-4eda-a986-1583e21087bd");
-         
-         // Initialize the GroupMembershipFileManager
-        GroupMembershipFileManager membershipFileManager = GroupMembershipFileManager.getInstance();
+        System.out.println("Adding posts to group1...");
+        groupContentManager1.addPost(post1);
+        groupContentManager1.addPost(post2);
 
-        // Read data from the file
-        membershipFileManager.readFromFile();
+        // Retrieve and print the posts for group1
+        System.out.println("Retrieving posts for group1...");
+        ArrayList<GroupPost> postsGroup1 = groupContentManager1.getPosts();
+        printPosts(postsGroup1);
 
-        // Get and print group memberships
-        System.out.println("Group Memberships:");
-        for (String groupId : membershipFileManager.getGroupsMembership().keySet()) {
-            JSONObject groupData = membershipFileManager.getUserData(groupId);
+        // Initialize GroupContentManager for group2
+        String groupId2 = "group2";
+        GroupContentManager groupContentManager2 = GroupContentManager.getInstance(groupId2);
 
-            System.out.println("---------------------------------");
-            System.out.println("Group ID: " + groupData.getString("groupId"));
-            System.out.println("Creator ID: " + groupData.getString("creatorId"));
+        // Add new posts to group2
+        GroupPost post3 = new GroupPost(groupId2, "user3", "This is a test post 1 for group2", "path/to/image3.jpg", LocalDateTime.now());
+        GroupPost post4 = new GroupPost(groupId2, "user4", "This is a test post 2 for group2", "path/to/image4.jpg", LocalDateTime.now());
 
-            JSONArray members = groupData.getJSONArray("members");
-            System.out.println("Members: " + members);
+        System.out.println("Adding posts to group2...");
+        groupContentManager2.addPost(post3);
+        groupContentManager2.addPost(post4);
 
-            JSONArray admins = groupData.getJSONArray("admins");
-            System.out.println("Admins: " + admins);
-        }
+        // Retrieve and print the posts for group2
+        System.out.println("Retrieving posts for group2...");
+        ArrayList<GroupPost> postsGroup2 = groupContentManager2.getPosts();
+        printPosts(postsGroup2);
 
-        Group g=GroupsFileManager.getInstance().getGroupById("group123");
-        GroupUser u;
-        
-        // Example: Add a new member to a group and save changes
-//        JSONObject groupData = membershipFileManager.getUserData("group123");
-//        if (groupData != null) {
-//            groupData.getJSONArray("members").put("user127");
-//            membershipFileManager.updateUserData("group123", groupData);
-//            membershipFileManager.saveToFile();
-//            System.out.println("Added new member to group123 and saved changes.");
-//        }
-    }
-        
+        // Reinitialize the manager to test persistence
+        GroupContentManager newGroupContentManager1 = GroupContentManager.getInstance(groupId1);
+        System.out.println("Retrieving posts for group1 after reinitializing...");
+        ArrayList<GroupPost> newPostsGroup1 = newGroupContentManager1.getPosts();
+        printPosts(newPostsGroup1);
+
+        GroupContentManager newGroupContentManager2 = GroupContentManager.getInstance(groupId2);
+        System.out.println("Retrieving posts for group2 after reinitializing...");
+        ArrayList<GroupPost> newPostsGroup2 = newGroupContentManager2.getPosts();
+        printPosts(newPostsGroup2);
     }
 
+    private static void printPosts(ArrayList<GroupPost> posts) {
+        System.out.println("Current posts:");
+        for (GroupPost post : posts) {
+            System.out.println("GroupId: " + post.getGroupId() + 
+                               ", UserId: " + post.getAuthorId() + 
+                               ", TextContent: " + post.getContentTxt() + 
+                               ", ImagePath: " + post.getcontentPath() + 
+                               ", Time: " + post.getUploadingTime());
+        }
+    }
+
+    private static void clearFile() {
+        try {
+            java.nio.file.Files.write(java.nio.file.Paths.get("group_posts.json"), new byte[0]);
+            System.out.println("Cleared the posts file for a fresh start.");
+        } catch (IOException e) {
+            System.out.println("Error clearing the file: " + e.getMessage());
+        }
+    }
+}
